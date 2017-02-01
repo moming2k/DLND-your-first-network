1b9dc09c099ed2af8cbe0d813014d6b005345cca
Chris Chan <chris.chan@gtomato.com>
2017-02-01 19:46:19 +0800

    try another half

--- try.py
+++ try.py
@@ -21,3 +21,10 @@ fields_to_drop = ['instant', 'dteday', 'season', 'weathersit',
 data = rides.drop(fields_to_drop, axis=1)
 data.head()
 
+quant_features = ['casual', 'registered', 'cnt', 'temp', 'hum', 'windspeed']
+# Store scalings in a dictionary so we can convert back later
+scaled_features = {}
+for each in quant_features:
+    mean, std = data[each].mean(), data[each].std()
+    scaled_features[each] = [mean, std]
+    data.loc[:, each] = (data[each] - mean)/std
